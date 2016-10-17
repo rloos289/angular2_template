c2b56733e3f7bce87db2f12907690b0559d18ed4
Ryan Loos <RLoos289@gmail.com>
2016-10-16 22:24:26 -0700

    make template

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Componet } from '@angular/core';
+
+@Component Annotations ({
+  selector: 'my-app',
+  template: '
+  <h1>My First Angular 2 App</h1>
+  '
+})
+
+export class AppComponent {
+
+}
